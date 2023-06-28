e62164832ac9384cec2cb31a68a37a8a472688b6
刘宇然 <liuyuran719829142@hotmail.com>
2023-06-28 17:10:18 +0800

    add some block properties

---
+++ minecraft-base/Components/Storage.cs
@@ -0,0 +1,10 @@
+﻿using Base.Interface;
+
+namespace Base.Components {
+    /// <summary>
+    /// 存储器，可以用作背包、箱子、仓库等
+    /// </summary>
+    public class Storage: IComponentData {
+        //
+    }
+}
--- minecraft-base/Generators/ChunkGenerator.cs
+++ minecraft-base/Generators/ChunkGenerator.cs
@@ -22,7 +22,7 @@ namespace Base.Generators {
             for (var x = 0; x < ParamConst.ChunkSize; x++) {
                 for (var y = 0; y < ParamConst.ChunkSize; y++) {
                     for (var z = 0; z < ParamConst.ChunkSize; z++) {
-                        chunk.SetBlock(x, y, z, new Bedrock());
+                        chunk.SetBlock(x, y, z, new Stone());
                     }
                 }
             }
@@ -54,7 +54,7 @@ namespace Base.Generators {
                     var target = Math.Floor(noise * ParamConst.ChunkSize);
                     for (var y = 0; y < ParamConst.ChunkSize; y++) {
                         if (y < target) 
-                            chunk.SetBlock(x, y, z, new Bedrock());
+                            chunk.SetBlock(x, y, z, new Stone());
                         else 
                             chunk.SetBlock(x, y, z, new Air());
                     }
