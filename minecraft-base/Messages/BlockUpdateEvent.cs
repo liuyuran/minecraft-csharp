589a9cff3e3037c43465475ea6e55bd9869e32a1
刘宇然 <liuyuran719829142@hotmail.com>
2023-07-03 15:03:58 +0800

    add block action event

---
+++ minecraft-base/Messages/BlockUpdateEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using Base.Interface;
+using Base.Utils;
+
+namespace Base.Messages {
+    public class BlockUpdateEvent: GameEvent {
+        public enum ActionTypeEnum {
+            Dig = 0,
+            Action = 1,
+        }
+        public Vector3 ChunkPos = Vector3.Zero;
+        public Vector3 BlockPos = Vector3.Zero;
+        public Direction Direction = Direction.up;
+        public ActionTypeEnum ActionType = 0;
+        public string Operand = "";
+    }
+}
