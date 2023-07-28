e9ec943820f4e504e88c308ea0fa943a414b2bec
刘宇然 <liuyuran719829142@hotmail.com>
2023-07-28 17:42:16 +0800

    add time manager

---
+++ minecraft-base/Manager/TimeManager.cs
@@ -0,0 +1,21 @@
+﻿namespace Base.Manager {
+    /// <summary>
+    /// 游戏内时间管理器
+    /// </summary>
+    public class TimeManager {
+        public static TimeManager Instance { get; } = new();
+        public long Tick { get;private set; }
+
+        public void Update() {
+            Tick++;
+        }
+        
+        public void Reset() {
+            Tick = 0;
+        }
+        
+        public void SetTick(long tick) {
+            Tick = tick;
+        }
+    }
+}
---
+++ minecraft-base/Systems/TickIncreaseSystem.cs
@@ -0,0 +1,14 @@
+﻿using Base.Interface;
+using Base.Manager;
+
+namespace Base.Systems {
+    public class TickIncreaseSystem: SystemBase {
+        public override void OnCreate() {
+            //
+        }
+
+        public override void OnUpdate() {
+            TimeManager.Instance.Update();
+        }
+    }
+}
